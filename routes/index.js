066d4e61cbf227b6836fa0fb3aeecd2b7be6a826
Mojiashen1 <mojiashen1@gmail.com>
2016-09-10 12:01:00 -0400

    twilio init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var twilio = require('./twilio');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(req.query.user);
+router.get('/disease', function(req, res, next) {
+  res.send("{'disease': '" + req.query.disease + "'}");
 });
+
+router.get('/sendMessage', function(req, res, next) {
+  var message = req.body.message;
+  var number = req.body.number;
+  twilio.sendTextMessages(number, message);
+});
+
 
 module.exports = router;
