cd65a1aca4688de63200c48848ccfb3a4c471965
Mojiashen1 <mojiashen1@gmail.com>
2016-09-10 01:13:59 -0400

    push to heroku

---
+++ models/models.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+// var findOrCreate = require('mongoose-findorcreate')
+
+// Create a connect.js inside the models/ directory that
+// exports your MongoDB URI!
+var connect = process.env.MONGODBURI;
+
+mongoose.connect(connect);
+
+var userSchema = mongoose.Schema({
+  symptom: String,
+  disease: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Disease'
+  }],
+  medicine: String,
+  doctor: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Doctor'
+  }],
+});
+
+var doctorSchema = mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+var diseaseSchema = mongoose.Schema({
+  name: String,
+  symptom: Number,
+  medicine: String
+});
+
+// userSchema.plugin(findOrCreate);
+module.exports = {User: mongoose.model('User', userSchema),
+Doctor: mongoose.model('Doctor', doctorSchema),
+Disease: mongoose.model('Disease', diseaseSchema)};
